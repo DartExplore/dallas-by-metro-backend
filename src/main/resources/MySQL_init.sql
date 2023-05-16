49684d57bb2fa74bcd1fe8803e86faa23962daf2
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-16 18:59:35 -0500

    Issue#71 (#72)
    * Made updates to Postgres_init.sql
    
    Removed color table
    Removed color as a foreign key on station_color
    Removed query statement at the end for the color table
    
    * Updated MySQL_init.sql to match Postgres_init.sql
    
    * spacing
    
    * Removed IF NOT EXISTS check as the previous statement is drop table

--- src/main/resources/MySQL_init.sql
+++ src/main/resources/MySQL_init.sql
@@ -7,21 +7,17 @@ CREATE TABLE station
 (
     station_id INT PRIMARY KEY,
     name       VARCHAR(255),
-    location   VARCHAR(255)
+    latitude   DOUBLE,
+    longitude  DOUBLE
 );
 
 INSERT INTO station (station_id, name)
-VALUES (1, 'A');
-INSERT INTO station (station_id, name)
-VALUES (2, 'B');
-INSERT INTO station (station_id, name)
-VALUES (3, 'C');
-INSERT INTO station (station_id, name)
-VALUES (4, 'D');
-INSERT INTO station (station_id, name)
-VALUES (5, 'E');
-INSERT INTO station (station_id, name)
-VALUES (6, 'F');
+VALUES (1, 'A'),
+       (2, 'B'),
+       (3, 'C'),
+       (4, 'D'),
+       (5, 'E'),
+       (6, 'F');
 
 DROP TABLE IF EXISTS points_of_interest;
 CREATE TABLE points_of_interest
@@ -37,19 +33,13 @@ CREATE TABLE points_of_interest
 );
 
 INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (1, 'PA1', 1);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (2, 'PB1', 2);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (3, 'PC1', 3);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (4, 'PD1', 4);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (5, 'PE1', 5);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (6, 'PE1', 5);
-INSERT INTO points_of_interest (poi_id, name, station_id)
-VALUES (7, 'PF1', 6);
+VALUES (1, 'PA1', 1),
+       (2, 'PB1', 2),
+       (3, 'PC1', 3),
+       (4, 'PD1', 4),
+       (5, 'PE1', 5),
+       (6, 'PE1', 5),
+       (7, 'PF1', 6);
 
 DROP TABLE IF EXISTS amenity;
 CREATE TABLE amenity
@@ -59,68 +49,44 @@ CREATE TABLE amenity
 );
 
 INSERT INTO amenity (amenity_id, amenity)
-VALUES (1, 'A1');
-INSERT INTO amenity (amenity_id, amenity)
-VALUES (2, 'A2');
-INSERT INTO amenity (amenity_id, amenity)
-VALUES (3, 'A3');
-
-DROP TABLE IF EXISTS color;
-CREATE TABLE color
-(
-    color VARCHAR(16) PRIMARY KEY,
-    CONSTRAINT color_check CHECK (color IN ('BLUE', 'RED', 'ORANGE', 'GREEN'))
-);
-
-INSERT INTO color
-VALUES ('BLUE');
-INSERT INTO color
-VALUES ('RED');
-INSERT INTO color
-VALUES ('ORANGE');
-INSERT INTO color
-VALUES ('GREEN');
+VALUES (1, 'A1'),
+       (2, 'A2'),
+       (3, 'A3');
 
 DROP TABLE IF EXISTS station_connection;
 CREATE TABLE station_connection
 (
     station1_id INT,
     station2_id INT,
-    color       VARCHAR(16),
-    PRIMARY KEY (station1_id, station2_id, color),
+    PRIMARY KEY (station1_id, station2_id),
     FOREIGN KEY (station1_id) REFERENCES station (station_id),
-    FOREIGN KEY (station2_id) REFERENCES station (station_id),
-    FOREIGN KEY (color) REFERENCES color (color)
+    FOREIGN KEY (station2_id) REFERENCES station (station_id)
 );
 
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (1, 2, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (2, 1, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (2, 3, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (2, 3, 'BLUE');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (3, 2, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (3, 2, 'BLUE');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (3, 4, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (3, 4, 'BLUE');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (4, 3, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (4, 3, 'BLUE');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (4, 5, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (5, 4, 'RED');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (4, 6, 'BLUE');
-INSERT INTO station_connection (station1_id, station2_id, color)
-VALUES (6, 4, 'BLUE');
+INSERT INTO station_connection (station1_id, station2_id)
+VALUES (1, 2),
+       (2, 3),
+       (3, 4),
+       (4, 5),
+       (4, 6);
+
+DROP TABLE IF EXISTS station_color;
+CREATE TABLE station_color
+(
+    station_id INT,
+    color      VARCHAR(16),
+    PRIMARY KEY (station_id, color),
+    FOREIGN KEY (station_id) REFERENCES station (station_id)
+);
+
+INSERT INTO station_color (station_id, color)
+VALUES (1, 'RED'),
+       (2, 'RED'),
+       (2, 'BLUE'),
+       (3, 'RED'),
+       (3, 'BLUE'),
+       (4, 'RED'),
+       (5, 'BLUE');
 
 DROP TABLE IF EXISTS poi_amenity;
 CREATE TABLE poi_amenity
@@ -133,30 +99,22 @@ CREATE TABLE poi_amenity
 );
 
 INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (1, 1);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (2, 2);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (3, 1);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (4, 1);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (4, 3);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (5, 1);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (5, 2);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (6, 2);
-INSERT INTO poi_amenity (poi_id, amenity_id)
-VALUES (7, 3);
+VALUES (1, 1),
+       (2, 2),
+       (3, 1),
+       (4, 1),
+       (4, 3),
+       (5, 1),
+       (5, 2),
+       (6, 2),
+       (7, 3);
 
 SELECT *
 FROM station;
 SELECT *
-FROM color;
-SELECT *
 FROM station_connection;
+SELECT *
+FROM station_color;
 SELECT *
 FROM points_of_interest;
 SELECT *
