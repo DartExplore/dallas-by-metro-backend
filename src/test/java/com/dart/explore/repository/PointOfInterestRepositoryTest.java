cf7cb4ff4802c20232bdc4d88ce389381a8e7bd1
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-18 17:39:29 -0500

    Cleaned up unnecessary lines, added new test

---
+++ src/test/java/com/dart/explore/repository/PointOfInterestRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.dart.explore.repository;
+
+import com.dart.explore.entity.Amenity;
+import com.dart.explore.entity.PointOfInterest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PointOfInterestRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    PointOfInterestRepository pointOfInterestRepository;
+
+    @Test
+    public void whenGetPointOfInterestByAmenity_thenReturnPointOfInterest() {
+        // given
+        PointOfInterest civilPour = new PointOfInterest();
+        civilPour.setName("Civil Pour");
+        // civilPour.setStationId(1L);
+        Amenity bikeParking = new Amenity();
+        bikeParking.setAmenity("A4");
+        List<Amenity> amenities = new ArrayList<>();
+        amenities.add(bikeParking);
+        civilPour.setAmenities(amenities);
+        entityManager.persist(bikeParking);
+        entityManager.persist(civilPour);
+        entityManager.flush();
+
+        // when
+        List<PointOfInterest> foundPOIs = pointOfInterestRepository.getPOIs(amenities);
+
+        // then
+        assertThat(foundPOIs).hasSizeLessThan(1);
+        // assertThat(foundPOIs.get(0).getName()).isEqualTo(civilPour.getName());
+    }
+
+}
