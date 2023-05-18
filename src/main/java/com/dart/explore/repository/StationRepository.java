f8b80efe7debcf73511cf316e02878ad7fac76ef
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-17 22:54:29 -0500

    Set empty list and getstationsbycolor

--- src/main/java/com/dart/explore/repository/StationRepository.java
+++ src/main/java/com/dart/explore/repository/StationRepository.java
@@ -5,12 +5,19 @@ import com.dart.explore.entity.Station;
 import com.dart.explore.entity.StationColor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 public interface StationRepository extends CrudRepository<Station, Integer> {
     // TODO
-    List<Station> getStationsByLine(StationColor color);
+    List<Station> getStationsByColor(StationColor color);
+
+    default List<Station> getStationsByLine(final StationColor color) {
+        return getStationsByColor(color);
+    }
 
     // TODO
-    List<PointOfInterest> getPOIsByStation(String stationName);
+    default List<PointOfInterest> getPOIsByStation(String stationName) {
+        return Collections.emptyList();
+    }
 }
