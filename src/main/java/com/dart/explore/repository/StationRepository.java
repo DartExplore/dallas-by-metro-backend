992c2758ede2d0426090f79ae74e82ec5c9339ad
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-18 12:03:09 -0500

    Updated to use the @query annotation

--- src/main/java/com/dart/explore/repository/StationRepository.java
+++ src/main/java/com/dart/explore/repository/StationRepository.java
@@ -3,17 +3,22 @@ package com.dart.explore.repository;
 import com.dart.explore.entity.PointOfInterest;
 import com.dart.explore.entity.Station;
 import com.dart.explore.entity.StationColor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public interface StationRepository extends CrudRepository<Station, Integer> {
     // TODO
-    List<Station> getStationsByColor(StationColor color);
+    @Query("SELECT s FROM Station s JOIN s.color c WHERE c = :color")
+    List<Station> findByColor(@Param("color") StationColor color);
 
     default List<Station> getStationsByLine(final StationColor color) {
-        return getStationsByColor(color);
+        return findByColor(color);
     }
 
     // TODO
