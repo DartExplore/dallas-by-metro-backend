7da95307cd6f46c50a99ef8c4fb46bc5d545569d
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-18 20:47:46 -0500

    Attempt at completing getPOIs repository

--- src/main/java/com/dart/explore/repository/PointOfInterestRepository.java
+++ src/main/java/com/dart/explore/repository/PointOfInterestRepository.java
@@ -2,14 +2,18 @@ package com.dart.explore.repository;
 
 import com.dart.explore.entity.Amenity;
 import com.dart.explore.entity.PointOfInterest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PointOfInterestRepository extends CrudRepository<PointOfInterest, Long> {
     // TODO
+    @Query("SELECT p FROM PointOfInterest p JOIN p.amenities a WHERE a IN :amenities")
+    List<PointOfInterest> findByAmenitiesIn(List<Amenity> amenities);
+
     default List<PointOfInterest> getPOIs(List<Amenity> amenities) {
-        return (List<PointOfInterest>) findAll();
+        return findByAmenitiesIn(amenities);
     }
 
     // TODO
