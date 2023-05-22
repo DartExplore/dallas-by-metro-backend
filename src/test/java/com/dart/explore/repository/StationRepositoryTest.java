970bc195e9f72f352d3cdda96db6031e5c272b96
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-22 16:28:33 -0500

    Updated to name and mocked importService to fix error

--- src/test/java/com/dart/explore/repository/StationRepositoryTest.java
+++ src/test/java/com/dart/explore/repository/StationRepositoryTest.java
@@ -2,11 +2,13 @@ package com.dart.explore.repository;
 
 import com.dart.explore.entity.Station;
 import com.dart.explore.entity.StationColor;
+import com.dart.explore.service.ImportService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
@@ -21,20 +23,25 @@ public class StationRepositoryTest {
     @Autowired
     private StationRepository stationRepository;
 
+    @MockBean
+    private ImportService importService;
+
     @Test
     public void whenFindByColor_thenReturnStations() {
         // given
-        Station grandCentral = new Station();
-        grandCentral.setName("Grand Central");
-        grandCentral.getColor().add(StationColor.GREEN);
-        entityManager.persist(grandCentral);
+        Station testStation = new Station();
+        testStation.setName("This is a test station");
+        testStation.getColor().add(StationColor.GREEN);
+        entityManager.persist(testStation);
         entityManager.flush();
 
         // when
         List<Station> found = stationRepository.getStationsByLine(StationColor.GREEN);
 
         // then
-        assertThat(found).hasSize(1);
-        assertThat(found.get(0).getName()).isEqualTo(grandCentral.getName());
+        // tests that both the return and the add worked since the count is 18 + 1 we added
+        assertThat(found).hasSize(19);
+        // tests that we can data is coming back correctly since we can correctly match the name
+        assertThat(found.get(found.size() - 1).getName()).isEqualTo(testStation.getName());
     }
 }
