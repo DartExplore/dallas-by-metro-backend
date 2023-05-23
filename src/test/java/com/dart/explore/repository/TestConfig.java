a1bc020917abab044bf23e64407bddb5016041c4
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-23 16:25:08 -0500

    Added general shared database config

---
+++ src/test/java/com/dart/explore/repository/TestConfig.java
@@ -0,0 +1,24 @@
+package com.dart.explore.repository;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import com.dart.explore.service.ImportService;
+
+@Configuration
+public class TestConfig {
+
+    @Autowired
+    private ImportService importService;
+
+    @PostConstruct
+    public void setup() throws Exception {
+        if (!importService.isDataLoaded()) {
+            importService.loadData("station_colors.txt");
+            importService.loadData("station_connections.txt");
+            importService.loadData("stations.txt");
+        }
+    }
+}
+
