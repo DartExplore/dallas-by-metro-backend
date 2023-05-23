56e845791b88f39f228bc9f2400ee50bd978c0c1
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-22 21:00:32 -0500

    Fixed entities

--- src/main/java/com/dart/explore/entity/Amenity.java
+++ src/main/java/com/dart/explore/entity/Amenity.java
@@ -1,6 +1,11 @@
 package com.dart.explore.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
 import java.util.Objects;
 import java.util.Set;
 
--- src/main/java/com/dart/explore/entity/PointOfInterest.java
+++ src/main/java/com/dart/explore/entity/PointOfInterest.java
@@ -1,6 +1,15 @@
 package com.dart.explore.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import java.util.List;
 import java.util.Objects;
 
