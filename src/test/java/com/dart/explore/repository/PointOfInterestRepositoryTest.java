0b8b4de33c85a1a7bef5e07c419e7c8c9aa007ea
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-22 19:19:04 -0500

    Good bit of refactoring
    - used @BeforeEach to set up values
    - added new tests for whenGetPointOfInterestsByStationAndAmenities_thenReturnPointOfInterests

--- src/test/java/com/dart/explore/repository/PointOfInterestRepositoryTest.java
+++ src/test/java/com/dart/explore/repository/PointOfInterestRepositoryTest.java
@@ -2,7 +2,9 @@ package com.dart.explore.repository;
 
 import com.dart.explore.entity.Amenity;
 import com.dart.explore.entity.PointOfInterest;
+import com.dart.explore.entity.Station;
 import com.dart.explore.service.ImportService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PointOfInterestRepositoryTest {
+
     @Autowired
     private TestEntityManager entityManager;
 
@@ -27,31 +31,39 @@ class PointOfInterestRepositoryTest {
     @MockBean
     private ImportService importService;
 
-    @Test
-    public void whenGetPointOfInterestByAmenity_thenReturnPointOfInterest() {
-        // given
-        PointOfInterest testPOI = new PointOfInterest();
+    PointOfInterest testPOI;
+    Amenity testAmenity;
+    Station testStation;
+
+    @BeforeEach
+    public void setup() {
+        testPOI = new PointOfInterest();
         testPOI.setName("Test POI");
-        Amenity testAmenity = new Amenity();
+        testAmenity = new Amenity();
         testAmenity.setAmenity("Test Amenity");
+        testStation = new Station();
+        testStation.setName("Test Station");
+        testPOI.setStation(testStation);
         List<Amenity> amenities = new ArrayList<>();
         amenities.add(testAmenity);
         testPOI.setAmenities(amenities);
         entityManager.persist(testAmenity);
+        entityManager.persist(testStation);
         entityManager.persist(testPOI);
         entityManager.flush();
+    }
 
-        // when
-        List<PointOfInterest> foundPOIs = pointOfInterestRepository.getPOIsByAmenities(amenities);
-
-        /*
-        then
-        Unless/until we either create dummy POI or pull down a more mature database as of writing this, it's worth
-        assuming that the only POI will be what's created during test
-         */
-        // Confirms that the POI adding correctly
+    @Test
+    public void whenGetPointOfInterestByAmenity_thenReturnPointOfInterest() {
+        List<PointOfInterest> foundPOIs = pointOfInterestRepository.getPOIsByAmenities(Collections.singletonList(testAmenity));
         assertThat(foundPOIs).hasSize(1);
-        // Confirms that the data is being inserted correctly
+        assertThat(foundPOIs.get(0).getName()).isEqualTo(testPOI.getName());
+    }
+
+    @Test
+    public void whenGetPointOfInterestsByStationAndAmenities_thenReturnPointOfInterests() {
+        List<PointOfInterest> foundPOIs = pointOfInterestRepository.getPointOfInterestsByStationAndAmenities(testStation.getName(), Collections.singletonList(testAmenity));
+        assertThat(foundPOIs).hasSize(1);
         assertThat(foundPOIs.get(0).getName()).isEqualTo(testPOI.getName());
     }
 
