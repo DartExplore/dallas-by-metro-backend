8351315af1fe0891b8d2eea6416af1e09fcf64b8
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-05-28 21:38:01 -0500

    Added security packages to implement API key
    * Created Security Directory
    * Created ApiKeyFilter and SecurityConfig package

--- src/main/java/com/dart/explore/controller/StationController.java
+++ src/main/java/com/dart/explore/controller/StationController.java
@@ -18,25 +18,25 @@ public class StationController {
     @Autowired
     StationServiceImpl stationService;
 
-    @GetMapping(value = "/poi")
+    @GetMapping(value = "/public/poi")
     ResponseEntity<List<PointOfInterest>> getPOIs(@RequestBody List<Amenity> amenities) {
         List<PointOfInterest> pointOfInterestList = stationService.getPOIs(amenities);
         return new ResponseEntity<List<PointOfInterest>>(pointOfInterestList, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/station/{line}")
+    @GetMapping(value = "/public/station/{line}")
     ResponseEntity<List<Station>> getStationsByLine(@PathVariable String line) {
         List<Station> stations = stationService.getStationsByLine(StationColor.valueOf(line));
         return new ResponseEntity<List<Station>>(stations, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/poi/{station}")
+    @GetMapping(value = "/public/poi/{station}")
     ResponseEntity<List<PointOfInterest>> getPOIsByStation(@PathVariable String station) {
         List<PointOfInterest> pointOfInterestList = stationService.getPOIsByStation(station);
         return new ResponseEntity<List<PointOfInterest>>(pointOfInterestList, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/poi/{station}/amenity")
+    @GetMapping(value = "/public/poi/{station}/amenity")
     ResponseEntity<List<PointOfInterest>> getPOIsAtStation(@PathVariable String station, @RequestBody List<Amenity> amenities) {
         List<PointOfInterest> pointOfInterestList = stationService.getPOIsAtStation(station, amenities);
         return new ResponseEntity<List<PointOfInterest>>(pointOfInterestList, HttpStatus.OK);
---
+++ src/main/java/com/dart/explore/security/ApiKeyFilter.java
@@ -0,0 +1,46 @@
+package com.dart.explore.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    private static final String API_KEY_HEADER = "X-Api-Key";
+
+    private final String apiKey;
+
+    public ApiKeyFilter(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        if (shouldNotFilter(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            String requestApiKey = request.getHeader(API_KEY_HEADER);
+            if (apiKey != null && apiKey.equals(requestApiKey)) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                response.getWriter().write("Invalid API Key");
+            }
+        }
+    }
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().startsWith("/api/public");
+    }
+}
