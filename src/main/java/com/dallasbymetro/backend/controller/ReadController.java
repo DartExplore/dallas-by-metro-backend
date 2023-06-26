e22243e6a1034e25a42a11a5957378de28c21143
Tyler Russell <tyler.russell1@aol.com>
2023-06-26 16:25:11 -0500

    Added validations for CSV fields and improved error handling.

--- src/main/java/com/dallasbymetro/backend/controller/ReadController.java
+++ src/main/java/com/dallasbymetro/backend/controller/ReadController.java
@@ -9,6 +9,7 @@ import com.dallasbymetro.backend.service.AmenityService;
 import com.dallasbymetro.backend.service.PointOfInterestService;
 import com.dallasbymetro.backend.service.StationServiceImpl;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,26 +44,44 @@ public class ReadController {
             @RequestParam("ID")
             @Parameter(example = "1,2,3") String poiIdsString)
             throws DartExploreException {
-        String[] poiIdsStrings = poiIdsString.split(",");
-        List<PointOfInterestDTO> pointOfInterestList = pointOfInterestService.getPOIsByIds(Arrays.asList(poiIdsStrings));
+        List<Long> poiIdList;
+        try{
+            poiIdList = (poiIdsString.isEmpty()) ? new ArrayList<>() :
+                    Arrays.stream(poiIdsString.split(",")).map(Long::parseLong).collect(Collectors.toList());
+        }catch(Exception e){
+            throw new DartExploreException("POI input must be CSV numbers.");
+        }
+
+        List<PointOfInterestDTO> pointOfInterestList = pointOfInterestService.getPOIsByIds(poiIdList);
         return new ResponseEntity<>(pointOfInterestList, HttpStatus.OK);
     }
 
     @GetMapping(value = "/poi/amenity")
     ResponseEntity<List<PointOfInterestDTO>> getPOIs(@RequestParam("amenityIdList") String amenitiesString) throws DartExploreException {
         // probably move this first bit to a utility class later
-        List<Long> amenityIdList = (amenitiesString.isEmpty()) ? new ArrayList<>() :
-                Arrays.stream(amenitiesString.split(",")).map(Long::parseLong).collect(Collectors.toList());
+        List<Long> amenityIdList;
+        try{
+            amenityIdList = (amenitiesString.isEmpty()) ? new ArrayList<>() :
+                    Arrays.stream(amenitiesString.split(",")).map(Long::parseLong).collect(Collectors.toList());
+        }catch(Exception e){
+            throw new DartExploreException("Amenities input must be CSV numbers.");
+        }
+
 
         List<PointOfInterestDTO> pointOfInterestList = stationService.getPOIs(amenityIdList);
         return new ResponseEntity<List<PointOfInterestDTO>>(pointOfInterestList, HttpStatus.OK);
     }
 
     @GetMapping(value = "/poi/{stationId}/amenity")
-    ResponseEntity<List<PointOfInterestDTO>> getPOIsAtStation(@PathVariable Long stationId, @RequestParam("amenityIdList") String amenitiesString) {
+    ResponseEntity<List<PointOfInterestDTO>> getPOIsAtStation(@PathVariable Long stationId, @RequestParam("amenityIdList") String amenitiesString) throws DartExploreException {
         // probably move this first bit to a utility class later
-        List<Long> amenityIdList = (amenitiesString.isEmpty()) ? new ArrayList<>() :
-                Arrays.stream(amenitiesString.split(",")).map(Long::parseLong).collect(Collectors.toList());
+        List<Long> amenityIdList;
+        try{
+            amenityIdList = (amenitiesString.isEmpty()) ? new ArrayList<>() :
+                    Arrays.stream(amenitiesString.split(",")).map(Long::parseLong).collect(Collectors.toList());
+        }catch(Exception e){
+            throw new DartExploreException("Amenities input must be CSV numbers.");
+        }
 
         List<Amenity> amenities = stationService.getAmenitiesById(amenityIdList);
         List<PointOfInterestDTO> pointOfInterestList = stationService.getPOIsAtStation(stationId, amenities);
