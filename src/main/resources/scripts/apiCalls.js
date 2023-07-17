b8f4e05dc8bad851127b8adc501b8df4142e1605
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-07-17 16:23:46 -0500

    improve test data insert

--- src/main/resources/scripts/apiCalls.js
+++ src/main/resources/scripts/apiCalls.js
@@ -19,42 +19,67 @@ const POIS = [
         poiId: 1,
         name: "Civil Pour", location: "8061 Walnut Hill Ln # 924, Dallas, TX 75231",
         walkingDistance: 3, picUrl: "coffee.png", type: "COFFEE_SHOP", stationId: 33,
-        amenities: [AMENITIES[1], AMENITIES[2], AMENITIES[3], AMENITIES[4]]
+        amenities: ["OUTDOOR_SPACE", "INDOOR_SPACE", "WI_FI", "ALCOHOL"]
     },
     {
         poiId: 2,
         name: "Dallas Museum of Art", location: "Dallas Museum of Art, 1717 N Harwood St, Dallas, TX 75201",
         walkingDistance: 9, picURL: "museum.png", type: "MUSEUM", stationId: 5,
-        amenities: [AMENITIES[0], AMENITIES[2]]
+        amenities: ["BIKE_PARKING", "INDOOR_SPACE"]
     },
     {
         poiId: 3,
         name: "The Dallas World Aquarium", location: "The Dallas World Aquarium, 1801 N Griffin St, Dallas, TX 75202",
         walkingDistance: 6, picUrl: "aquarium.png", type: "AQUARIUM", stationId: 15,
-        amenities: [AMENITIES[2]]
+        amenities: ["INDOOR_SPACE"]
     },
     {
         poiId: 4,
         name: "AT&T Discovery District", location: "AT&T Discovery District, 308 S Akard St, Dallas, TX 75202",
         walkingDistance: 5, picUrl: "park.png", type: "PARK", stationId: 15,
-        amenities: [AMENITIES[0], AMENITIES[1], AMENITIES[2]]
+        amenities: ["BIKE_PARKING", "OUTDOOR_SPACE", "INDOOR_SPACE"]
     },
     {
         poiId: 5,
         name: "American Airlines Center", location: "American Airlines, 2500 Victory Ave, Dallas, TX 75219",
         walkingDistance: 4, picUrl: "stadium.png", type: "STADIUM", stationId: 29,
-        amenities: [AMENITIES[2]]
+        amenities: ["INDOOR_SPACE"]
     }
 ];
 
-POIS.map(
-    (poi) => (
-        axios.post('http://localhost:8080/api/private/poi', poi, {headers})
-            .then(response =>
-                console.log("POI " + poi.poiId + " inserted successfully.")
-            )
-            .catch(error =>
-                console.log('Error:', error.message)
-            )
-    )
-);
+const addAmenities = async (amenities) => {
+    for (const amenity of amenities) {
+        await axios.post('http://localhost:8080/api/private/amenity', amenity, {headers})
+            .then(response => console.log("Amenity " + response.data.amenityId + " inserted successfully."))
+            .catch(error => console.log(error.response.data));
+    }
+}
+
+const getAllAmenities = async () => {
+    const response = await axios.get('http://localhost:8080/api/public/amenities', {headers});
+    return response.data;
+};
+
+const addPOIs = async (pois) => {
+    for (const poi of pois) {
+        await axios.post('http://localhost:8080/api/private/poi', poi, {headers})
+            .then(response => console.log("POI " + poi.poiId + " inserted successfully."))
+            .catch(error => console.log(error.response.data));
+    }
+}
+
+// Execution
+(async () => {
+    await addAmenities(AMENITIES);
+    const amenitiesFromAPI = await getAllAmenities();
+    const amenitiesByName = amenitiesFromAPI.reduce((acc, curr) => {
+        acc[curr.amenity] = curr;
+        return acc;
+    }, {});
+
+    POIS.forEach(poi => {
+        poi.amenities = poi.amenities.map(a => amenitiesByName[a]);
+    });
+
+    await addPOIs(POIS);
+})();
